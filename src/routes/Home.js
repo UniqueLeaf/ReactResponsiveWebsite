8a79954ce1ad36d43d494de1c63eb7203ccb411f
DIALOGGROUP\4937 <yifei.chen@icemedia.com.au>
2022-08-26 15:06:25 +1000

    update

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
-import Video from '../components/VideoHero'
+import Video from '../components/VideoHero';
+import Footer from '../components/Footer';
 
 const Home = () => {
     return (
         <div>
             <Navbar />
             <Video />
+            <Footer />
         </div>
     )
 }
