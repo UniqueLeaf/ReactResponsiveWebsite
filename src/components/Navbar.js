4ccb099aa83466d7932c639e27c0c4fbb75db69f
DIALOGGROUP\4937 <yifei.chen@icemedia.com.au>
2022-08-26 13:57:19 +1000

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,7 @@ const Navbar = () => {
         <div className='header'>
             
             <Link to='/'><h1>GLX TRVL</h1></Link>
-            <ul className='nav-menu'>
+            <ul className={click ? 'nav-menu active' : 'nav-menu'}>
                 <li>
                     <Link to='/'>Home</Link>
                 </li>
@@ -26,8 +26,9 @@ const Navbar = () => {
                     <Link to='/contact'>Contact</Link>
                 </li>
             </ul>
-            <div className='hamburger'>
-                <FaBars size={20} style={{color: 'white'}}/>
+            <div className='hamburger' onClick={handleClick}>
+                {click ? (<FaTimes ize={20} style={{color: 'white'}}/>) : (<FaBars size={20} style={{color: 'white'}}/>)}
+                
             </div>
             
         </div>
